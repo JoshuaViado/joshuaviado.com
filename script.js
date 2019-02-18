6535f3c2f2bb1e19f6818f22c8cdf140d4143b7f
Joshua Viado <34035630+JoshuaViado@users.noreply.github.com>
2019-02-18 15:14:41 +0000

    Update script.js

--- script.js
+++ script.js
@@ -20,6 +20,3 @@ function contactsClicked(name) {
 }
 
 email.addEventListener("onmouseover", contactsClicked);
-
-//hiText.addEventListener("webkitAnimationEnd", textAppear);
-//hiText.addEventListener("animationend", textAppear);
