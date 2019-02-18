caa4739281353838fb6c1e153590b478010e0a15
Joshua Viado <34035630+JoshuaViado@users.noreply.github.com>
2019-02-18 15:14:23 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,6 @@
-const profile = document.getElementsByClassName("profile-container");
-var text = document.querySelector(".profile-text");
-var hiText = document.querySelector(".typing");
 const email = document.querySelector(".icon");
 const output = document.querySelector(".contacts-output");
 const output2 = document.querySelector(".contacts-output2");
-
-function setCoords(event) {
-  var x = event.clientX;
-  var y = event.clientY;
-
-  document.documentElement.style.setProperty("--cursorX", x + "px");
-  document.documentElement.style.setProperty("--cursorY", y + "px");
-  console.log(document.documentElement.style.getPropertyValue("--cursorX"));
-}
-
-function hoverContainer() {
-  document.documentElement.style.setProperty("--containerIndex", "-2");
-}
-
-function profileHover() {
-  profile.style.animation = "profile 0.7s";
-}
-
-function textAppear() {
-  text.style.opacity = "1";
-  console.log("opacity 1");
-}
 
 function contactsClicked(name) {
   if (name === "email") {
